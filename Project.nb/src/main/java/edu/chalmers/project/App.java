4fbf57979ffcb9f1a132342e40a89d679d53b5f8
Emilia Nilsson <eminil@student.chalmers.se>
2014-03-18 16:15:43 +0100

    moved project

---
+++ Project.nb/src/main/java/edu/chalmers/project/App.java
@@ -0,0 +1,18 @@
+package edu.chalmers.project;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println("Hello Git!");
+        for(int i=0; i<5; i++) {
+            System.out.println("Hej Joel!");
+            
+        }
+    }
+}
---
+++ Project.nb/src/main/java/edu/chalmers/project/Test.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package edu.chalmers.project;
+
+/**
+ *
+ * @author Mia
+ */
+public class Test {
+    //jbjkb
+}
