10655348a2cd97bc8ee38291352ac48b85f1e125
Oskar Jönefors <oskar.jonefors@gmail.com>
2014-03-27 16:46:46 +0100

    Added some stubbed classes.

---
+++ Blockster/core/src/main/java/edu/chalmers/Blockster/core/Stage.java
@@ -0,0 +1,20 @@
+package edu.chalmers.Blockster.core;
+
+import com.badlogic.gdx.maps.tiled.TiledMap;
+
+/**
+ * A class to represent a stage.
+ * @author Oskar Jönefors
+ *
+ */
+public class Stage {
+	private TiledMap map;
+	
+	public Stage(TiledMap map) {
+		this.map = map;
+	}
+	
+	public TiledMap getMap() {
+		return map;
+	}
+}
