2ce65664f3a2d75017565db7bbdbb2e3b2f6f302
Eric Bjuhr <Sweed.Raver@gmail.com>
2014-03-27 16:40:09 +0100

    Factory and Properties

---
+++ Blockster/core/src/main/java/edu/chalmers/Blockster/core/BlockProperties.java
@@ -0,0 +1,44 @@
+package edu.chalmers.Blockster.core;
+
+
+
+public class BlockProperties {
+
+	private String name;
+	
+	private String destructible;
+	
+	private String movable;
+	
+	public BlockProperties() {
+		
+	}
+	
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDestructible() {
+		return destructible;
+	}
+
+	public void setDestructible(String destructible) {
+		this.destructible = destructible;
+	}
+
+	public String getMovable() {
+		return movable;
+	}
+
+	public void setMovable(String movable) {
+		this.movable = movable;
+	}
+	
+	public class Textures {
+		
+	}
+}
