342edfda1e5710d91526eb929652c1f2f29b1911
Eric Bjuhr <Sweed.Raver@gmail.com>
2014-03-29 21:48:22 +0100

    fixed typo in enum Direction

--- Blockster/core/src/main/java/edu/chalmers/Blockster/core/Direction.java
+++ Blockster/core/src/main/java/edu/chalmers/Blockster/core/Direction.java
@@ -1,7 +1,7 @@
 package edu.chalmers.Blockster.core;
 
 public enum Direction {
-	LEFT(-1,0), RIGHT(1,0), CLIMB_LEFT(-1,1), CLIMB_RIGHT(-1,1), FALL(0,-1), NONE(0,0);
+	LEFT(-1,0), RIGHT(1,0), CLIMB_LEFT(-1,1), CLIMB_RIGHT(1,1), FALL(0,-1), NONE(0,0);
 	
 	public final int deltaX;
 	public final int deltaY;
