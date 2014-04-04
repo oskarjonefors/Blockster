1e770446312dffe2834f6a3f8310e22dfaaaeab9
Emilia Nilsson <eminil@student.chalmers.se>
2014-04-04 11:57:53 +0200

    Added a BlockMap interface with a method to return a BlockLayer

---
+++ Blockster/core/src/main/java/edu/chalmers/Blockster/core/BlockMap.java
@@ -0,0 +1,17 @@
+package edu.chalmers.Blockster.core;
+
+/**
+ * A interface to represent a map with layers.
+ * @author Mia
+ */
+public interface BlockMap {
+	
+	/**
+	 * A method to get a layer with blocks.
+	 * @param index specifies which block layer to return. Starts with 0.
+	 * @return the specified block layer.
+	 */
+	public BlockLayer getBlockLayer(int index) {
+		return blockLayer;
+	}
+}
