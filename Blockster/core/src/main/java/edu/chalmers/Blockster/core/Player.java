cfa00c283fa253bca9595d83330e4cac4936a9a8
Eric Bjuhr <Sweed.Raver@gmail.com>
2014-04-04 13:57:17 +0200

    Sorted methods by alphabetic order

--- Blockster/core/src/main/java/edu/chalmers/Blockster/core/Player.java
+++ Blockster/core/src/main/java/edu/chalmers/Blockster/core/Player.java
@@ -8,27 +8,27 @@ public interface Player {
 
 	public Vector2f getGravity();
 	
+	public float getHeight();
+	
 	public float getMaximumMovementSpeed();
 	
 	public Vector2f getVelocity();
+	
+	public float getWidth();
+	
+	public float getX();
+	
+	public float getY();
 	
 	public void increaseGravity(float deltaTime);
 	
 	public void move(Direction dir, float distance);
 	
 	public void resetGravity();
-	
+
 	public void setVelocityX(float velX);
 	
 	public void setVelocityY(float velX);
-	
-	public float getX();
-	
-	public float getY();
-
-	public float getWidth();
-	
-	public float getHeight();
 	
 	
 }
