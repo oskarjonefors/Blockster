b23c4ac2d40e47599e557dde94f5447958b4803c
Eric Bjuhr <Sweed.Raver@gmail.com>
2014-05-03 22:21:45 +0200

    Added new classes in preparation for refactoring

---
+++ Blockster/core/src/main/java/edu/chalmers/Blockster/core/BlockGrabbedInteraction.java
@@ -0,0 +1,19 @@
+package edu.chalmers.Blockster.core;
+
+import edu.chalmers.Blockster.core.util.Direction;
+
+public class BlockGrabbedInteraction implements PlayerInteraction {
+
+	@Override
+	public void interact(Direction dir) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void endInteraction() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
