946905eda592f0f2456f6b7c4bfe75276db851f4
Joel Tegman <gotinkx@hotmail.com>
2014-05-08 13:23:10 +0200

    changed in Factory

--- Blockster/core/src/main/java/edu/chalmers/Blockster/core/util/Factory.java
+++ Blockster/core/src/main/java/edu/chalmers/Blockster/core/util/Factory.java
@@ -1,12 +1,11 @@
 package edu.chalmers.Blockster.core.util;
 
-import edu.chalmers.Blockster.core.objects.BlockMap;
 import edu.chalmers.Blockster.core.objects.Player;
 
 public interface Factory {
 	
-	public BlockMap createMap();
+	public GridMap createMap();
 
-	public Player createPlayer(float startX, float startY, BlockMap blockLayer);
+	public Player createPlayer(float startX, float startY, GridMap blockLayer);
 
 }
