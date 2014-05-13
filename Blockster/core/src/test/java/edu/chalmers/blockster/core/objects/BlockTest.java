e8001d1235112846026069876f1ee1ae11aceba4
Emilia Nilsson <eminil@student.chalmers.se>
2014-05-13 15:08:40 +0200

    BlockTest has now all it's test methods implemented and runs smoothe

--- Blockster/core/src/test/java/edu/chalmers/blockster/core/objects/BlockTest.java
+++ Blockster/core/src/test/java/edu/chalmers/blockster/core/objects/BlockTest.java
@@ -1,7 +1,6 @@
 package edu.chalmers.blockster.core.objects;
 
 import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -14,13 +13,20 @@ public class BlockTest {
 	private BlockMap blockMap;
 	private Block block;
 	private AnimationState anim;
-
+	private Movement movement;
+	private int startX;
+	private int startY;
+	private String property;
+	
 	@Before
 	public void setUp() {
 		int[][] playerPositions = {{0,0}};
+		startX = 3;
+		startY = 2;
 		blockMap = new BlockMap(10, 10, 128, 128, playerPositions);
-		block = new Block(3,2, blockMap);
-		anim = new AnimationState(Movement.PULL_LEFT);
+		block = new Block(startX, startY, blockMap);
+		movement = Movement.PULL_LEFT;
+		anim = new AnimationState(movement);
 		
 		blockMap.insertBlock(block);
 	}
@@ -29,39 +35,96 @@ public class BlockTest {
 	public void testMoveToNextPosition() {
 		block.setAnimationState(anim);
 		block.moveToNextPosition();
-		assertTrue(Math.round(block.getX()) == 4);
+		assertTrue(Math.round(block.getX()) == movement.getDirection().deltaX + startX);
 	}
 
 	@Test
 	public void testSetAnimationState() {
 		block.setAnimationState(anim);
-		assertTrue(anim.getMovement() == Movement.PULL_RIGHT);
+		assertTrue(anim.getMovement() == movement);
 	}
 
 	@Test
 	public void testSetProperty() {
-		block.setProperty("liftable");
-		assertTrue(block.isLiftable());
+		boolean correct = true;
+		
+		//#1 Liftable
+		property = "liftable";
+		block.setProperty(property);
+		correct &= block.isLiftable();
+		
+		//#2 Movable
+		property = "movable";
+		block.setProperty(property);
+		correct &= block.isMovable();
+		
+		//#3 Weight
+		property = "weight";
+		block.setProperty(property);
+		correct &= block.hasWeight();
+		
+		//#4 Solid
+		property = "solid";
+		block.setProperty(property);
+		correct &= block.isSolid();
+		
+		assertTrue(correct);
 	}
 
 	@Test
 	public void testIsSolid() {
-		fail("Not yet implemented");
+		boolean correct = true;
+		
+		//#1 Non-solid block
+		correct &= !block.isSolid();
+		
+		//#2 Solid block
+		block.setProperty("solid");
+		correct &= block.isSolid();
+		
+		assertTrue(correct);
 	}
 
 	@Test
 	public void testIsLiftable() {
-		fail("Not yet implemented");
+		boolean correct = true;
+		
+		//#1 Non-liftable block
+		correct &= !block.isLiftable();
+		
+		//#2 Liftable block
+		block.setProperty("liftable");
+		correct &= block.isLiftable();
+		
+		assertTrue(correct);
 	}
 
 	@Test
 	public void testIsMovable() {
-		fail("Not yet implemented");
+		boolean correct = true;
+		
+		//#1 Non-movable block
+		correct &= !block.isMovable();
+		
+		//#2 Movable block
+		block.setProperty("movable");
+		correct &= block.isMovable();
+		
+		assertTrue(correct);
 	}
 
 	@Test
 	public void testHasWeight() {
-		fail("Not yet implemented");
+		boolean correct = true;
+		
+		//#1 Has no weight
+		correct &= !block.isSolid();
+		
+		//#2 Has weight
+		block.setProperty("weight");
+		correct &= block.hasWeight();
+		
+		assertTrue(correct);
 	}
 
 }
