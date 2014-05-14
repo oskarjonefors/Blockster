eefd088b9095a65883b0b49c8a8c42d1ca5169cf
Eric Bjuhr <Sweed.Raver@gmail.com>
2014-05-15 01:15:57 +0200

    Cleaned up imports etc, sorted members of MiniMap class

--- Blockster/core/src/main/java/edu/chalmers/blockster/core/gdx/view/MiniMap.java
+++ Blockster/core/src/main/java/edu/chalmers/blockster/core/gdx/view/MiniMap.java
@@ -9,7 +9,6 @@ import com.badlogic.gdx.graphics.Pixmap.Format;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.graphics.g2d.TextureRegion;
 
 import edu.chalmers.blockster.core.objects.ActiveBlockListener;
 import edu.chalmers.blockster.core.objects.Block;
@@ -21,30 +20,49 @@ public class MiniMap implements BlockMapListener, ActiveBlockListener {
 
 
 	
+	private class Bounds {
+		
+		public final float x, y, width, height;
+		
+		public Bounds(float x, float y, float width, float height) {
+			this.x = x;
+			this.y = y;
+			this.width = width;
+			this.height = height;
+		}
+		
+		public boolean contains(float x, float y) {
+			return x >= this.x - 3 && x < this.x + width + 3
+					&& y >= this.y - 3 && y < this.y + height + 3;
+		}
+		
+		
+	}
 	private final Set<Block> activeBlocks;
-	private final Set<Block> staticBlocks;
 	
+	private final Set<Block> staticBlocks;
 	private int scaleX;
-	private int scaleY;
 	
 
+	private int scaleY;
 	private final float offsetX = 16f;
+	
+	
 	private final float offsetY = 16f;
-	
-	
 	private Player activePlayer;
 	private final Sprite minimapSprite;
-	private Texture previousTexture;
 	
+	private Texture previousTexture;
 	private final int width, height;
 	private float[][] playerPos;
-	private float viewX, viewY, viewportWidth, viewportHeight;
 	
+	private float viewX, viewY, viewportWidth, viewportHeight;
 	public static final int NO_BLOCK = Color.rgba8888(0, 0, 0, 1f);
-	public static final int SOLID_BLOCK = Color.rgba8888(0.8f, 0.8f, 0.8f, 1f);
-	public static final int LIFTABLE_BLOCK = Color.rgba8888(0.8f, 0, 0, 1f); 
+	public static final int SOLID_BLOCK = Color.rgba8888(0.8f, 0.8f, 0.8f, 1f); 
+	public static final int LIFTABLE_BLOCK = Color.rgba8888(0.8f, 0, 0, 1f);
 	public static final int VIEWPORT = Color.rgba8888(1f, 1f, 1f, 0.15f);
 	public static final int ACTIVE_PLAYER = Color.rgba8888(1f, 1f, 0, 1f);
+	
 	public static final int INACTIVE_PLAYER = Color.rgba8888(0, 0, 1f, 1f);
 	
 	public MiniMap (int mapWidth, int mapHeight) {
@@ -123,6 +141,11 @@ public class MiniMap implements BlockMapListener, ActiveBlockListener {
 		}
 	}
 	
+	private void drawBackground(Pixmap pixmap) {
+		pixmap.setColor(NO_BLOCK);
+		pixmap.fill();
+	}
+	
 	private void drawPlayers(Pixmap pixmap, Bounds bounds) {
 		pixmap.setColor(INACTIVE_PLAYER);
 		for(int i = 0; i < playerPos.length; i++) {
@@ -153,20 +176,29 @@ public class MiniMap implements BlockMapListener, ActiveBlockListener {
 			}
 		}
 	}
+
+	private void drawViewport(Pixmap pixmap, Bounds bounds) {
+		pixmap.setColor(VIEWPORT);
+		int x = Math.round(viewX * scaleX);
+		int y = Math.round(viewY * scaleY);
+		int width = Math.round(viewportWidth * scaleX);
+		int height = Math.round(viewportHeight * scaleY);
+		
+		if (height + y >= this.height * scaleY) {
+			height = this.height * scaleY - y - 1;
+		}
+		
+		pixmap.fillRectangle(x, y, width, height);
+	}
 	
-	private Bounds getTextureBounds() {
-		//Make the side two times the offset long (one time for each side of the center)
-		float width = 2 * offsetX;
-		float height = 2 * offsetY;
-		
-		//Get the center point of the player, then subtract by offset
-		float x = (activePlayer.getX() + activePlayer.getWidth() / 2f) 
-				/ activePlayer.getScaleX() - offsetX;
-		float y = (activePlayer.getY() - activePlayer.getHeight() / 2f) 
-				/ activePlayer.getScaleY() + offsetY + 1;
-		
-		Bounds bounds = new Bounds(x, y, width, height);
-		return bounds;
+	private int getColor(Block block) {
+		if (block.isLiftable()) {
+			return LIFTABLE_BLOCK;
+		} else if (block.isSolid()) {
+			return SOLID_BLOCK;
+		} else {
+			return NO_BLOCK;
+		}
 	}
 	
 	private Bounds getDrawBounds() {
@@ -183,12 +215,12 @@ public class MiniMap implements BlockMapListener, ActiveBlockListener {
 		Bounds bounds = new Bounds(x, y, width, height);
 		return bounds;
 	}
-
+	
 	private Pixmap getMinimapPixmap(Bounds bounds) {
 		
 		
-		Pixmap pixmap = new Pixmap((int) (width*scaleX),
-				(int) (height*scaleY), Format.RGBA8888);
+		Pixmap pixmap = new Pixmap(width*scaleX,
+				height*scaleY, Format.RGBA8888);
 		
 		
 		drawBackground(pixmap);
@@ -198,21 +230,6 @@ public class MiniMap implements BlockMapListener, ActiveBlockListener {
 		drawViewport(pixmap, bounds);
 		
 		return pixmap;
-	}
-	
-	private void drawBackground(Pixmap pixmap) {
-		pixmap.setColor(NO_BLOCK);
-		pixmap.fill();
-	}
-	
-	private int getColor(Block block) {
-		if (block.isLiftable()) {
-			return LIFTABLE_BLOCK;
-		} else if (block.isSolid()) {
-			return SOLID_BLOCK;
-		} else {
-			return NO_BLOCK;
-		}
 	}
 	
 	private int getPixMapY(Pixmap pixmap, int y) {
@@ -227,20 +244,21 @@ public class MiniMap implements BlockMapListener, ActiveBlockListener {
 		return scaleY;
 	}
 	
-	private void drawViewport(Pixmap pixmap, Bounds bounds) {
-		pixmap.setColor(VIEWPORT);
-		int x = Math.round(viewX * scaleX);
-		int y = Math.round(viewY * scaleY);
-		int width = Math.round(viewportWidth * scaleX);
-		int height = Math.round(viewportHeight * scaleY);
+	private Bounds getTextureBounds() {
+		//Make the side two times the offset long (one time for each side of the center)
+		float width = 2 * offsetX;
+		float height = 2 * offsetY;
 		
-		if (height + y >= this.height * scaleY) {
-			height = this.height * scaleY - y - 1;
-		}
+		//Get the center point of the player, then subtract by offset
+		float x = (activePlayer.getX() + activePlayer.getWidth() / 2f) 
+				/ activePlayer.getScaleX() - offsetX;
+		float y = (activePlayer.getY() - activePlayer.getHeight() / 2f) 
+				/ activePlayer.getScaleY() + offsetY + 1;
 		
-		pixmap.fillRectangle(x, y, width, height);
+		Bounds bounds = new Bounds(x, y, width, height);
+		return bounds;
 	}
-	
+
 	private void prepareSprite(Sprite sprite, Texture texture) {
 		Bounds bounds = getTextureBounds();
 		float textX = bounds.x * scaleX;
@@ -255,10 +273,14 @@ public class MiniMap implements BlockMapListener, ActiveBlockListener {
 		sprite.setSize(Math.abs(textWidth), Math.abs(textHeight));
 	}
 
+	public void setActivePlayer(Player player) {
+		this.activePlayer = player;
+	}
+
 	public void setPlayerLocations(float[][] locations) {
 		playerPos = locations.clone();
 	}
-
+	
 	public void setScaleX(int scaleX) {
 		this.scaleX = scaleX;
 	}
@@ -267,35 +289,12 @@ public class MiniMap implements BlockMapListener, ActiveBlockListener {
 		this.scaleY = scaleY;
 	}
 	
-	public void setActivePlayer(Player player) {
-		this.activePlayer = player;
-	}
-
 	public void setViewportBounds(float x, float y, float width, float height) {
 
 		viewX = x;
 		viewY = -y;
 		viewportWidth = width;
 		viewportHeight = height;
-	}
-	
-	private class Bounds {
-		
-		public final float x, y, width, height;
-		
-		public Bounds(float x, float y, float width, float height) {
-			this.x = x;
-			this.y = y;
-			this.width = width;
-			this.height = height;
-		}
-		
-		public boolean contains(float x, float y) {
-			return x >= this.x - 3 && x < this.x + width + 3
-					&& y >= this.y - 3 && y < this.y + height + 3;
-		}
-		
-		
 	}
 	
 	
