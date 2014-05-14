1fec9faf42b8b19e8aae2393a9c7371fcb3fe268
Oskar Jönefors <oskar.jonefors@gmail.com>
2014-05-14 09:25:43 +0200

    Remove commented out code and reordered variable modifiers in Calculations.

--- Blockster/core/src/main/java/edu/chalmers/blockster/core/util/Calculations.java
+++ Blockster/core/src/main/java/edu/chalmers/blockster/core/util/Calculations.java
@@ -2,17 +2,9 @@ package edu.chalmers.blockster.core.util;
 
 public final class Calculations {
 	
-	public final static float STANDARD_MOVE_DURATION = 0.2f;
-	public final static float MOVE_LIFTED_BLOCK_DURATION = 0.15f;
-	public final static float BLOCK_FALL_DURATION = 0.05f;
-	/*public final static int CHECK_UP_FLAG = 1 << 0;
-	public final static int CHECK_UP_RIGHT_FLAG = 1 << 1;
-	public final static int CHECK_RIGHT_FLAG = 1 << 2;
-	public final static int CHECK_DOWN_RIGHT_FLAG = 1 << 3;
-	public final static int CHECK_DOWN_FLAG = 1 << 4;
-	public final static int CHECK_DOWN_LEFT_FLAG = 1 << 5;
-	public final static int CHECK_LEFT_FLAG = 1 << 6;
-	public final static int CHECK_UP_LEFT_FLAG = 1 << 7;*/
+	public static final float STANDARD_MOVE_DURATION = 0.2f;
+	public static final float MOVE_LIFTED_BLOCK_DURATION = 0.15f;
+	public static final float BLOCK_FALL_DURATION = 0.05f;
 	
 	private Calculations() {
 		
@@ -64,63 +56,6 @@ public final class Calculations {
 		return isSolid(blockLayer, (int) ((player.getX() + player.getWidth()) / tileWidth),
 				(int) ((player.getY() + player.getHeight()) / tileHeight));
 	}
-	
-	/*public static boolean collisionSurroundingBlocks(PhysicalObject pos,
-			 GridMap blockLayer, int flags) {
-		boolean collision = false;
-		final float scaleX = pos.getScaleX();
-		final float scaleY = pos.getScaleY();
-		
-		if ((flags & CHECK_UP_FLAG) != 0) {
-			collision |= isSolid(blockLayer,
-					(int) (pos.getX() / scaleX), 
-					(int) (pos.getY() / scaleY) + 1);
-		}
-		
-		if ((flags & CHECK_UP_RIGHT_FLAG) != 0) {
-			collision |= isSolid(blockLayer,
-					(int) (pos.getX() / scaleX) + 1, 
-					(int) (pos.getY() / scaleY) + 1);
-		}
-		
-		if ((flags & CHECK_RIGHT_FLAG) != 0) {
-			collision |= isSolid(blockLayer,
-					(int) (pos.getX() / scaleX) + 1, 
-					(int) (pos.getY() / scaleY));
-		}
-		
-		if ((flags & CHECK_DOWN_RIGHT_FLAG) != 0) {
-			collision |= isSolid(blockLayer,
-					(int) (pos.getX() / scaleX) + 1, 
-					(int) (pos.getY() / scaleY) - 1);
-		}
-		
-		if ((flags & CHECK_DOWN_FLAG) != 0) {
-			collision |= isSolid(blockLayer,
-					(int) (pos.getX() / scaleX), 
-					(int) (pos.getY() / scaleY) - 1);
-		}
-		
-		if ((flags & CHECK_DOWN_LEFT_FLAG) != 0) {
-			collision |= isSolid(blockLayer,
-					(int) (pos.getX() / scaleX) - 1, 
-					(int) (pos.getY() / scaleY) - 1);
-		}
-
-		if ((flags & CHECK_LEFT_FLAG) != 0) {
-			collision |= isSolid(blockLayer,
-					(int) (pos.getX() / scaleX) - 1, 
-					(int) (pos.getY() / scaleY));
-		}
-
-		if ((flags & CHECK_UP_LEFT_FLAG) != 0) {
-			collision |= isSolid(blockLayer,
-					(int) (pos.getX() / scaleX) - 1, 
-					(int) (pos.getY() / scaleY) + 1);
-		}
-		
-		return collision;
-	}*/
 	
 	private static boolean isSolid(GridMap blockLayer, int x, int y) {
 		return blockLayer.hasBlock(x, y) && blockLayer.getBlock(x,y).isSolid();
