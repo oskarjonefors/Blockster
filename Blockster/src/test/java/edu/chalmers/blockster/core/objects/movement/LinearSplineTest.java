beca30f1fe5ae447d8df6fa7906c450a3d40dbcb
Eric Bjuhr <Sweed.Raver@gmail.com>
2014-05-16 15:00:11 +0200

    Removed unnecessary import from BezierSplineTest and finished LinearSplineTest

--- Blockster/src/test/java/edu/chalmers/blockster/core/objects/movement/LinearSplineTest.java
+++ Blockster/src/test/java/edu/chalmers/blockster/core/objects/movement/LinearSplineTest.java
@@ -2,23 +2,44 @@ package edu.chalmers.blockster.core.objects.movement;
 
 import static org.junit.Assert.*;
 
+import javax.vecmath.Vector2f;
+
 import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class LinearSplineTest {
 
+	private float diff = (float) Math.pow(10, -5);
+	private static LinearSpline spline;
+	private static Direction dir;
+	
 	@BeforeClass
-	public static void setUpBeforeClass() throws Exception {
+	public static void setUp() {
+		dir = Direction.LEFT;
+		spline = new LinearSpline(dir);
 	}
 
 	@Test
 	public void testGetPosition() {
-		fail("Not yet implemented");
+		float p1 = 0, p2 = 50f, p3 = 100;
+		Vector2f pos1 = spline.getPosition(p1);
+		Vector2f pos2 = spline.getPosition(p2);
+		Vector2f pos3 = spline.getPosition(p3);
+		
+		float distStart = pos1.length();
+		float distMiddle = pos2.length();
+		float distEnd = pos3.length();
+		
+		boolean test1 = Math.abs(distStart) < diff;
+		boolean test2 = distStart < distMiddle && distMiddle < distEnd;
+		boolean test3 = Math.abs(Math.abs(distEnd) - Math.sqrt(2)) < diff;
+		
+		assertTrue(test1 && test2 && test3);
 	}
 
 	@Test
 	public void testGetDirection() {
-		fail("Not yet implemented");
+		assertTrue(dir == spline.getDirection());
 	}
 
 }
