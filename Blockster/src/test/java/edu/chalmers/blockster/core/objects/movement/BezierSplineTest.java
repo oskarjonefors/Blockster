f99c972196ee79803aa788357fa926aaf649d782
Eric Bjuhr <Sweed.Raver@gmail.com>
2014-05-16 16:56:41 +0200

    Added more coverage to BezierSplineTest

--- Blockster/src/test/java/edu/chalmers/blockster/core/objects/movement/BezierSplineTest.java
+++ Blockster/src/test/java/edu/chalmers/blockster/core/objects/movement/BezierSplineTest.java
@@ -10,17 +10,10 @@ import org.junit.Test;
 public class BezierSplineTest {
 
 	private float diff = (float) Math.pow(10, -5);
-	private static BezierSpline spline;
-	private static Direction dir;
-	
-	@BeforeClass
-	public static void setUp() {
-		dir = Direction.UP_RIGHT;
-		spline = new BezierSpline(dir);
-	}
 
 	@Test
-	public void testGetPosition() {
+	public void testGetPositionOne() {
+		BezierSpline spline = new BezierSpline(Direction.UP_LEFT);
 		float p1 = 0, p2 = 50f, p3 = 100;
 		Vector2f pos1 = spline.getPosition(p1);
 		Vector2f pos2 = spline.getPosition(p2);
@@ -33,12 +26,35 @@ public class BezierSplineTest {
 		boolean test1 = distStart < diff;
 		boolean test2 = distStart < distMiddle && distMiddle < distEnd;
 		boolean test3 = Math.abs(distEnd - Math.sqrt(2)) < diff;
+		boolean lifting = Math.abs(pos2.y) > Math.abs(pos2.x);
 		
-		assertTrue(test1 && test2 && test3);
+		assertTrue(test1 && test2 && test3 && lifting);
 	}
 
 	@Test
+	public void testGetPositionTwo() {
+		BezierSpline spline = new BezierSpline(Direction.DOWN_RIGHT);
+		float p1 = 0, p2 = 50f, p3 = 100;
+		Vector2f pos1 = spline.getPosition(p1);
+		Vector2f pos2 = spline.getPosition(p2);
+		Vector2f pos3 = spline.getPosition(p3);
+		
+		float distStart = Math.abs(pos1.length());
+		float distMiddle = Math.abs(pos2.length());
+		float distEnd = Math.abs(pos3.length());
+		
+		boolean test1 = distStart < diff;
+		boolean test2 = distStart < distMiddle && distMiddle < distEnd;
+		boolean test3 = Math.abs(distEnd - Math.sqrt(2)) < diff;
+		boolean falling = Math.abs(pos2.x) > Math.abs(pos2.y);
+		
+		assertTrue(test1 && test2 && test3 && falling);
+	}
+	
+	@Test
 	public void testGetDirection() {
+		Direction dir = Direction.UP_LEFT;
+		BezierSpline spline = new BezierSpline(dir);
 		assertTrue(dir == spline.getDirection());
 	}
 
