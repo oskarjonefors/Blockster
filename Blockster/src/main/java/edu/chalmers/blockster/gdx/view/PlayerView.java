87572b104fdfaa04f487611e26d41bbb0748ab90
Joel Tegman <gotinkx@hotmail.com>
2014-05-24 19:54:08 +0200

    Animations for falling now working

--- Blockster/src/main/java/edu/chalmers/blockster/gdx/view/PlayerView.java
+++ Blockster/src/main/java/edu/chalmers/blockster/gdx/view/PlayerView.java
@@ -25,17 +25,17 @@ public class PlayerView {
 	private final Map<Direction, Animation> walkAnimations;
 	private float animTime;
 	private Movement lastMovement;
-	
+
 	public PlayerView(Player player, Map<Movement, Animation> arrayOfAnimation,
 			Map<Direction, Animation> walkAnimations) {
 		this.player = player;
 		this.arrayOfAnimation = arrayOfAnimation;
 		this.walkAnimations = walkAnimations;
-		
+
 		final String prefix = player.getWorld() == World.DAY ? "" : "night_";
 		final Texture standPic = new Texture ("Animations/" + prefix + "stand.png");
 		final TextureRegion[][] standPics = TextureRegion.split(standPic, standPic.getWidth(), standPic.getHeight()/2);
-		
+
 		standLeft = standPics[1][0];
 		standRight = standPics[0][0];
 
@@ -64,11 +64,11 @@ public class PlayerView {
 
 
 	public TextureRegion chooseAnimation(){
-		
+
 		final AnimationState animState = player.getAnimationState();
 		final Movement movement = animState.getMovement();
 		animTime += Gdx.graphics.getDeltaTime();
-		
+
 		if (!player.isGrabbingBlock() && movement == Movement.NONE) {
 			if (player.isMoving()) {
 				return getWalkingPic();
@@ -83,7 +83,7 @@ public class PlayerView {
 	private TextureRegion getAnimations(Movement movement) {
 		if (movement == Movement.PUSH_LEFT || movement == Movement.PUSH_RIGHT) {
 			return arrayOfAnimation.get(movement).getKeyFrame(animTime, true);
-	  
+
 		} else if (player.isGrabbingBlock() && movement != Movement.PULL_LEFT && movement != Movement.PULL_RIGHT) {
 			if (lastMovement == Movement.GRAB_LEFT) {
 				player.setDirection(Direction.LEFT);
@@ -92,46 +92,49 @@ public class PlayerView {
 				player.setDirection(Direction.RIGHT);
 				return arrayOfAnimation.get(Movement.GRAB_LEFT).getKeyFrame(animTime);
 			}
+		} else if (movement == Movement.FALL_DOWN)  {
+			Movement move = player.getDirection() == Direction.LEFT ? Movement.MOVE_LEFT : Movement.MOVE_RIGHT;
+			return arrayOfAnimation.get(move).getKeyFrame(animTime, true);
 		} else {
 			return arrayOfAnimation.get(movement).getKeyFrame(animTime, true);
 		}
 	}
-				
-	private TextureRegion getWalkingPic() {
-		lastMovement = player.getDirection() == Direction.LEFT ? Movement.GRAB_LEFT : Movement.GRAB_RIGHT;
-		return walkAnimations.get(player.getDirection()).getKeyFrame(animTime, true);
-	}
 
-	private TextureRegion getStillPic() {
-		if (player.isLiftingBlock()) {
-			return player.getDirection() == Direction.LEFT ? 
-					arrayOfAnimation.get(Movement.MOVE_LEFT).getKeyFrame(animTime) :
-						arrayOfAnimation.get(Movement.MOVE_RIGHT).getKeyFrame(animTime);
-		} else {
-		return player.getDirection() == Direction.LEFT ? standLeft : standRight;
+		private TextureRegion getWalkingPic() {
+			lastMovement = player.getDirection() == Direction.LEFT ? Movement.GRAB_LEFT : Movement.GRAB_RIGHT;
+			return walkAnimations.get(player.getDirection()).getKeyFrame(animTime, true);
 		}
-	}
 
-	public TextureRegion getCurrentAnimation(AnimationState anim, Float time){
-		return arrayOfAnimation.get(anim.getMovement()).getKeyFrame(time, true);
-	}
+		private TextureRegion getStillPic() {
+			if (player.isLiftingBlock()) {
+				return player.getDirection() == Direction.LEFT ? 
+						arrayOfAnimation.get(Movement.MOVE_LEFT).getKeyFrame(animTime) :
+							arrayOfAnimation.get(Movement.MOVE_RIGHT).getKeyFrame(animTime);
+			} else {
+				return player.getDirection() == Direction.LEFT ? standLeft : standRight;
+			}
+		}
 
-	public final void setSize(float width, float height) {
-		sprite.setSize(width, height);
-		player.setWidth(width);
-		player.setHeight(width);
-	}
+		public TextureRegion getCurrentAnimation(AnimationState anim, Float time){
+			return arrayOfAnimation.get(anim.getMovement()).getKeyFrame(time, true);
+		}
 
-	public Player getPlayer() {
-		return player;
-	}
+		public final void setSize(float width, float height) {
+			sprite.setSize(width, height);
+			player.setWidth(width);
+			player.setHeight(width);
+		}
 
-	public float getX() {
-		return player.getX();
-	}
+		public Player getPlayer() {
+			return player;
+		}
 
-	public float getY() {
-		return player.getY();
-	}
+		public float getX() {
+			return player.getX();
+		}
 
-}
+		public float getY() {
+			return player.getY();
+		}
+
+	}
