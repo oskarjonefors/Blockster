24a141fb53088ef9ca1e5386e0869fe3e62e5514
Oskar Jönefors <oskar.jonefors@gmail.com>
2014-05-24 19:42:36 +0200

    Removed modifiers implied by the context in BlockMap.

--- Blockster/src/main/java/edu/chalmers/blockster/core/objects/BlockMap.java
+++ Blockster/src/main/java/edu/chalmers/blockster/core/objects/BlockMap.java
@@ -8,17 +8,17 @@ import edu.chalmers.blockster.core.util.GridMap;
 
 public interface BlockMap extends GridMap{
 
-	public abstract void addActiveBlockListener(ActiveBlockListener listener);
+	void addActiveBlockListener(ActiveBlockListener listener);
 
-	public abstract List<ActiveBlockListener> getActiveBlockListener();
+	List<ActiveBlockListener> getActiveBlockListener();
 
-	public abstract void removeActiveBlockListener(ActiveBlockListener listener);
+	void removeActiveBlockListener(ActiveBlockListener listener);
 
-	public abstract void addListener(BlockMapListener listener);
+	void addListener(BlockMapListener listener);
 
-	public abstract List<BlockMapListener> getListeners();
+	List<BlockMapListener> getListeners();
 
-	public abstract void removeListener(BlockMapListener listener);
+	void removeListener(BlockMapListener listener);
 
 	/**
 	 * Replaces the specific block with an empty block in the block map, i.e.
@@ -27,7 +27,7 @@ public interface BlockMap extends GridMap{
 	 * @param block
 	 *            The block that is to be removed.
 	 */
-	public abstract void removeBlock(Block block);
+	void removeBlock(Block block);
 
 	/**
 	 * Inserts the specific block with it's coordinates in the block map. Alerts
@@ -36,35 +36,35 @@ public interface BlockMap extends GridMap{
 	 * @param block
 	 *            That is to be inserted.
 	 */
-	public abstract void insertBlock(Block block);
+	void insertBlock(Block block);
 
 	/**
 	 * Get the width of the block in pixels.
 	 * 
 	 * @return Block width in pixels.
 	 */
-	public abstract float getBlockWidth();
+	float getBlockWidth();
 
 	/**
 	 * Get the height of the block in pixels.
 	 * 
 	 * @return Block height in pixels.
 	 */
-	public abstract float getBlockHeight();
+	float getBlockHeight();
 
 	/**
 	 * Get the height of the layer in blocks.
 	 * 
 	 * @return The width of the layer in blocks.
 	 */
-	public abstract int getHeight();
+	int getHeight();
 
 	/**
 	 * Get the width of the layer in blocks.
 	 * 
 	 * @return The width of the layer in blocks.
 	 */
-	public abstract int getWidth();
+	int getWidth();
 
 	/**
 	 * Insert the given block into the layer at the given coordinates.
@@ -75,7 +75,7 @@ public interface BlockMap extends GridMap{
 	 *            Y coordinate
 	 * @param block
 	 */
-	public abstract void setBlock(int x, int y, Block block);
+	void setBlock(int x, int y, Block block);
 
 	/**
 	 * Get the Block at the given coordinates, or null if there is none.
@@ -86,7 +86,7 @@ public interface BlockMap extends GridMap{
 	 *            Y coordinate
 	 * @return A Block, or null if no Block is found at the given coordinates.
 	 */
-	public abstract Block getBlock(int x, int y);
+	Block getBlock(int x, int y);
 
 	/**
 	 * Check to see if there is a block at the given coordinates.
@@ -97,11 +97,11 @@ public interface BlockMap extends GridMap{
 	 *            Y coordinate to check
 	 * @return True if there is a block at (x,y), otherwise false.
 	 */
-	public abstract boolean hasBlock(int x, int y);
+	boolean hasBlock(int x, int y);
 
-	public abstract Set<Block> getBlocks();
+	Set<Block> getBlocks();
 
-	public abstract Set<Block> getActiveBlocks();
+	Set<Block> getActiveBlocks();
 
 	/**
 	 * Get a float array of the player starting positions in the map. They are
@@ -110,10 +110,10 @@ public interface BlockMap extends GridMap{
 	 * 
 	 * @return
 	 */
-	public abstract List<Point> getPlayerStartingPositions();
+	List<Point> getPlayerStartingPositions();
 
-	public abstract void updateActiveBlocks(float deltaTime);
+	void updateActiveBlocks(float deltaTime);
 
-	public abstract void addActiveBlock(Block block);
+	void addActiveBlock(Block block);
 
 }
