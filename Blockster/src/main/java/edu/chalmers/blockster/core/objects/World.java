795ffd6499423fc9debd936eb2d2b6ca6d11969b
Oskar Jönefors <oskar.jonefors@gmail.com>
2014-05-25 16:09:34 +0200

    Added comment.

--- Blockster/src/main/java/edu/chalmers/blockster/core/objects/World.java
+++ Blockster/src/main/java/edu/chalmers/blockster/core/objects/World.java
@@ -1,5 +1,10 @@
 package edu.chalmers.blockster.core.objects;
 
+/**
+ * An enum representing the two different world in Blockster. One for each player.
+ * @author Oskar Jönefors
+ *
+ */
 public enum World {
 	DAY, NIGHT;
 }
